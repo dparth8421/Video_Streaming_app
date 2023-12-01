6ad85bb4f7e43d0d82f92ceddcc0bf6a50eb6a9b
dparth8421 <dparth388@gmail.com>
2023-12-01 22:19:31 +0530

    added VideoCards &&added constants && Fetched videos from API

---
+++ src/components/VideoCards.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const VideoCards = ({ info }) => {
+  console.log(info);
+  const { snippet, statistics } = info;
+  const { channelTitle, title, thumbnails } = snippet;
+  return (
+    <div className="p-2 m-2 w-72 shadow-lg">
+      <img className="rounded-lg" src={thumbnails.medium.url} alt="Thumbnail" />
+      <ul>
+        <li className="font-bold py-2">{title}</li>
+        <li>{channelTitle}</li>
+        <li>{statistics.viewCount} Views</li>
+      </ul>
+    </div>
+  );
+};
+
+export default VideoCards;
